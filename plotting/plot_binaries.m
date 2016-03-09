0fe7ad33efaafb8b6f12c71c65c9975f9ba66e1a
Gideon Dresdner <gideond@gmail.com>
2016-03-09 15:47:34 -0500

    wip, what does gmints do?

--- plotting/plot_binaries.m
+++ plotting/plot_binaries.m
@@ -5,7 +5,7 @@ function plot_binary(noninterpolated_filename, results_filename,
                      truth_filename, no_genes=50) 
   load(results_filename);
   Z0 = load_truth_Immuno(truth_filename);
-  [x0, pos, ~, ~] = load_noninterpolated_Immuno(noninterpolated_filename, no_genes);
+  [x0, pos, gmints, ~] = load_noninterpolated_Immuno(noninterpolated_filename, no_genes);
 
   pos = pos - 5000;
   ## double check I've sorted
@@ -27,6 +27,7 @@ function plot_binary(noninterpolated_filename, results_filename,
   d = length(w);
   clf;
   gmints(length(gmints)+1)=length(x0)+1;
+  keyboard
   for c = 1:3
     figure;
     for i = 1:d
@@ -34,8 +35,10 @@ function plot_binary(noninterpolated_filename, results_filename,
       plot(pos(gmints(c):gmints(c+1)-1), Z0(gmints(c):gmints(c+1)-1, i),'ro');
       hold on
       plot(pos(gmints(c):gmints(c+1)-1), Z(gmints(c):gmints(c+1)-1, i),'b*');
-      ##         legend('truth','estimated', 'Location','southeast');
-      ##        title(sprintf('binary curve %d with weight %0.2f', i, num2str(w(i))));
+
+      ## legend('truth','estimated', 'Location','southeast');
+      ## title(sprintf('binary curve %d with weight %0.2f', i, num2str(w(i))));
+
       axis([-5000,5000,-0.1,1.1]);
     end
   end
