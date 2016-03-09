3d5997567e0dba4a7b87557948eabf322a73e3ab
Gideon Dresdner <gideond@gmail.com>
2016-03-09 15:35:48 -0500

    decided to just take the longer one.
    pull in more code.

--- plotting/plot_binaries.m
+++ plotting/plot_binaries.m
@@ -11,12 +11,35 @@ function plot_binary(noninterpolated_filename, results_filename,
   ## double check I've sorted
   [w, I] = sort(w);
   Z = Z(:, I);
-  keyboard
-  Z0= Z0(1:size(Z,1),1:size(Z,2));
-  assert(isequal(size(Z),size(Z0)))
-  diff_ratio = sum(sum(abs(Z-Z0))) / prod(size(Z))
 
-  
+  ## TODO is this right? The old code assumes that size(Z,1) <
+  ## size(Z0,1) always...
+  if size(Z,1) < size(Z0,1)
+    Z0= Z0(1:size(Z,1),1:size(Z,2));
+  else
+    Z= Z(1:size(Z0,1),1:size(Z0,2));
+  end
+  assert(isequal(size(Z),size(Z0)))
+
+  diff_ratio = sum(sum(abs(Z-Z0))) / prod(size(Z));
+
+  ## Plot the true vs. estimate binaries of the first gene
+  d = length(w);
+  clf;
+  gmints(length(gmints)+1)=length(x0)+1;
+  for c = 1:3
+    figure;
+    for i = 1:d
+      subplot(d,1,i);
+      plot(pos(gmints(c):gmints(c+1)-1), Z0(gmints(c):gmints(c+1)-1, i),'ro');
+      hold on
+      plot(pos(gmints(c):gmints(c+1)-1), Z(gmints(c):gmints(c+1)-1, i),'b*');
+      ##         legend('truth','estimated', 'Location','southeast');
+      ##        title(sprintf('binary curve %d with weight %0.2f', i, num2str(w(i))));
+      axis([-5000,5000,-0.1,1.1]);
+    end
+  end
+
 end
 
 ##
