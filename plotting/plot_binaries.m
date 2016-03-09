1d55bf1a6f77c3025fdd6e37469361ef00aff456
Gideon Dresdner <gideond@gmail.com>
2016-03-09 16:56:37 -0500

    plot_binaries works again

--- plotting/plot_binaries.m
+++ plotting/plot_binaries.m
@@ -2,7 +2,7 @@ addpath('../utils/');
 
 ## filenames are arrays of the form ["path" "to" "file" "filename"]
 function plot_binary(noninterpolated_filename, results_filename,
-                     truth_filename, no_genes=50) 
+                     truth_filename, no_genes) 
   load(results_filename);
   Z0 = load_truth_Immuno(truth_filename);
   [x0, pos, gmints, ~] = load_noninterpolated_Immuno(noninterpolated_filename, no_genes);
@@ -27,7 +27,6 @@ function plot_binary(noninterpolated_filename, results_filename,
   d = length(w);
   clf;
   gmints(length(gmints)+1)=length(x0)+1;
-  keyboard
   for c = 1:3
     figure;
     for i = 1:d
@@ -36,46 +35,21 @@ function plot_binary(noninterpolated_filename, results_filename,
       hold on
       plot(pos(gmints(c):gmints(c+1)-1), Z(gmints(c):gmints(c+1)-1, i),'b*');
 
-      ## legend('truth','estimated', 'Location','southeast');
-      ## title(sprintf('binary curve %d with weight %0.2f', i, num2str(w(i))));
+      legend('truth','estimated', 'Location','southeast');
+      title(sprintf('binary curve %d with weight %0.2f', i, num2str(w(i))));
 
       axis([-5000,5000,-0.1,1.1]);
     end
   end
+  keyboard
 
 end
 
 ##
-## example
+## example:
 ##
-
-datadir = "/home/gideon/Data/methylation-decomposition/ImmunoMix3/";
-
-plot_binary([datadir, 'Noise_depth_20_sample_8.cleaned.txt'],
-            '/home/gideon/Data/methylation-decomposition-results/hmm3.0_Immuno/20_8.mat',
-            [datadir, 'foobar.csv']);
-            ## [datadir, 'Solutions_OneThousandGene_8.txt']);
-
-## pos = pos - 5000;
-## % double check I'v sorted
-## [w, I] = sort(w);
-## Z = Z(:, I);
-## Z0= Z0(1:size(Z,1),1:size(Z,2));
-## assert(isequal(size(Z),size(Z0)))
-## diff_ratio = sum(sum(abs(Z-Z0))) / prod(size(Z))
-## % Plot the true vs. estimate binaries of the first gene
-## d = length(w);
-## clf;
-## gmints(length(gmints)+1)=length(x0)+1;
-## for c = 1:3
-##     figure;
-##     for i = 1:d
-##         subplot(d,1,i);
-##         plot(pos(gmints(c):gmints(c+1)-1), Z0(gmints(c):gmints(c+1)-1, i),'ro');
-##         hold on
-##         plot(pos(gmints(c):gmints(c+1)-1), Z(gmints(c):gmints(c+1)-1, i),'b*');
-## %         legend('truth','estimated', 'Location','southeast');
-## %         title(sprintf('binary curve %d with weight %0.2f', i, num2str(w(i))));
-##         axis([-5000,5000,-0.1,1.1]);
-##     end
-## end
+## plot_binary('Noise_depth_20_sample_8.cleaned.txt',
+##             '20_8.mat',
+##             'Solutions_OneThousandGene_8.cleaned.txt',
+##             50);
+##
--- utils/load_noninterpolated_Immuno.m
+++ utils/load_noninterpolated_Immuno.m
@@ -1,22 +1,29 @@
-function [x0, pos, gmints, gmnames] = load_noninterpolated_Immuno(dataset, no_genes=50)
+function [x0, pos, gmints, gmnames] = load_noninterpolated_Immuno(dataset, no_genes)
 
 raw = load(dataset);
 
-gmints = raw(:,1);
+geneno = raw(:,1);
 pos = raw(:, 2) + 5000;
 x0 = raw(:, 3);
+
+[gmnames, gmints] = unique(geneno);
 
 % only take first `no_genes` genes
 if nargin > 1
   % don't know where the gene numbers are starting, so shift them to 1.
-  mask = (gmints - min(gmints) + 1) < no_genes;
+  mask = (geneno - min(geneno) + 1) < no_genes;
+
   x0 = x0(mask);
   pos = pos(mask);
+
+  gmnames = gmnames(1:no_genes);
+  gmints = gmints(1:no_genes);
 end
+
 end
 
 ##
 ## example:
 ##
-## load_noninterpolated_Immuno("path/to/file.cleaned.txt");
+## load_noninterpolated_Immuno("Noise_depth_20_sample_8.cleaned.txt");
 ##
